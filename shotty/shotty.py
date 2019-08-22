95c8d3dfc0ec3e258972edb8562db165b973a927
GrayOctober <ffstorm@gmx.com>
2019-08-22 11:58:42 -0400

    changed to function

--- shotty/shotty.py
+++ shotty/shotty.py
@@ -1,6 +1,8 @@
-fav_colors = {'karl': 'blue', 'elizabeth': 'red', 'elouise': 'pretty in pink'}
+def greet_user(): 
+    fav_colors = {'karl': 'blue', 'elizabeth': 'red', 'elouise': 'pretty in pink'}
+    for k,v in fav_colors.items():
+        print (k + " : " + v)
 
-for k,v in fav_colors.items():
-    print (k + " : " + v)
+greet_user()
   
 
